14e23109a4e38b1d73fe65b956bfab1b9718ac4e
Samuel <samuelyongw@gmail.com>
2023-03-10 01:34:07 +1100

    private set to false on package.json

--- src/utils/motion.ts
+++ src/utils/motion.ts
@@ -116,7 +116,7 @@ export const textVariant2 = {
   },
 };
 
-export const fadeIn = (direction, type, delay, duration) => ({
+export const fadeIn = (direction: string, type: string, delay: number, duration: number) => ({
   hidden: {
     x: direction === "left" ? 100 : direction === "right" ? -100 : 0,
     y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
@@ -141,56 +141,8 @@ export const fadeIn = (direction, type, delay, duration) => ({
   },
 });
 
-export const planetVariants = (direction) => ({
-  hidden: {
-    x: direction === "left" ? "-100%" : "100%",
-    rotate: 120,
-  },
-  show: {
-    x: 0,
-    rotate: 0,
-    transition: {
-      type: "spring",
-      duration: 1.8,
-      delay: 0.5,
-    },
-  },
-});
 
-export const zoomIn = (delay, duration) => ({
-  hidden: {
-    scale: 0,
-    opacity: 0,
-  },
-  show: {
-    scale: 1,
-    opacity: 1,
-    transition: {
-      type: "tween",
-      delay,
-      duration,
-      ease: "easeOut",
-    },
-  },
-});
 
-export const footerVariants = {
-  hidden: {
-    opacity: 0,
-    y: 50,
-    transition: {
-      type: "spring",
-      stiffness: 300,
-      damping: 140,
-    },
-  },
-  show: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      type: "spring",
-      stiffness: 80,
-      delay: 0.5,
-    },
-  },
-};
+
+
+
